f0973bb884baff7aa987e013a52f6bebdd79481e
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2019-07-29 12:33:50 -0300

    Add missing networks

--- truffle.js
+++ truffle.js
@@ -2,8 +2,8 @@
  * https://github.com/aragon/aragonOS/blob/v4.0.0/truffle-config.js
  */
 
-require('babel-register')
-require('babel-polyfill')
+require('@babel/register')
+require('@babel/polyfill')
 
 const homedir = require('homedir')
 const path = require('path')
@@ -13,68 +13,77 @@ const HDWalletProviderPrivkey = require('truffle-hdwallet-provider-privkey')
 
 const DEFAULT_MNEMONIC = 'explain tackle mirror kit van hammer degree position ginger unfair soup bonus'
 
-const defaultRPC = (network) =>
-    `https://${network}.infura.io`
+const defaultRPC = network => `https://${network}.infura.io`
 
-const configFilePath = (filename) =>
-    path.join(homedir(), `.aragon/${filename}`)
+const configFilePath = filename => path.join(homedir(), `.aragon/${filename}`)
 
 const mnemonic = () => {
-    try {
-        return require(configFilePath('mnemonic.json')).mnemonic
-    } catch (e) {
-        return DEFAULT_MNEMONIC
-    }
+  try {
+    return require(configFilePath('mnemonic.json')).mnemonic
+  } catch (e) {
+    return DEFAULT_MNEMONIC
+  }
 }
 
-const settingsForNetwork = (network) => {
-    try {
-        return require(configFilePath(`${network}_key.json`))
-    } catch (e) {
-        return {}
-    }
+const settingsForNetwork = network => {
+  try {
+    return require(configFilePath(`${network}_key.json`))
+  } catch (e) {
+    return {}
+  }
 }
 
 // Lazily loaded provider
-const providerForNetwork = (network) => (
-    () => {
-        let {rpc, keys} = settingsForNetwork(network)
-        rpc = rpc || defaultRPC(network)
+const providerForNetwork = network => () => {
+  let { rpc, keys } = settingsForNetwork(network)
+  rpc = rpc || defaultRPC(network)
 
-        if (!keys || keys.length == 0) {
-            return new HDWalletProvider(mnemonic(), rpc)
-        }
+  if (!keys || keys.length === 0) {
+    return new HDWalletProvider(mnemonic(), rpc)
+  }
 
-        return new HDWalletProviderPrivkey(keys, rpc)
-    }
-)
+  return new HDWalletProviderPrivkey(keys, rpc)
+}
 module.exports = {
-    networks: {
-        development: {
-            host: 'localhost',
-            port: 8545,
-            network_id: '*'
-        },
-        mainnet: {
-            network_id: 1,
-            provider: providerForNetwork('mainnet')
-        },
-        rinkeby: {
-            network_id: 4,
-            provider: providerForNetwork('rinkeby')
-        }
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
     },
-    compilers: {
-        solc: {
-            version: '0.4.24', // Fetch exact version from solc-bin (default: truffle's version)
-            // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-            settings: {
-                // See the solidity docs for advice about optimization and evmVersion
-                optimizer: {
-                    enabled: true,
-                    runs: 200,
-                },
-            },
-        },
+    rpc: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '15',
     },
+    coverage: {
+      host: 'localhost',
+      network_id: '*',
+      port: 8555,
+      gas: 0xffffffffff,
+      gasPrice: 0x01,
+    },
+    rinkeby: {
+      network_id: 4,
+      provider: providerForNetwork('rinkeby'),
+    },
+    mainnet: {
+      network_id: 1,
+      provider: providerForNetwork('mainnet'),
+    },
+  },
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: '0.4.24', // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
 }
